27229fa46e039095a978277c6fd07d7a3c3457fa
witrdotnet <witr.net@gmail.com>
2015-12-13 09:47:01 +0100

    compile and install user erlang scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,20 @@ if [ ! -f "$TSUNG_WORKSPACE/$TSUNG_CONFIG_FILE" ]; then
 	exit 1
 fi
 
+echo "start ssh..."
 service ssh start
+
+cd $TSUNG_WORKSPACE
+if ls $TSUNG_WORKSPACE/*.erl 1> /dev/null 2>&1; then
+	echo "compile erlang scripts..."
+	erlc $TSUNG_WORKSPACE/*.erl
+	echo "compile erlang scripts OK"
+fi
+
+if ls $TSUNG_WORKSPACE/*.beam 1> /dev/null 2>&1; then
+	echo "copy beam scripts..."
+	cp $TSUNG_WORKSPACE/*.beam /usr/lib/erlang/lib/tsung-1.6.0/ebin/
+fi
+
+echo "run tsung..."
 tsung -f $TSUNG_WORKSPACE/$TSUNG_CONFIG_FILE -k start
